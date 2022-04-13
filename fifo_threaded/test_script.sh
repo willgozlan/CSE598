a8dfa62056051804e8909943d1865a04b2d479a0
william.gozlan <william.gozlan@RDSH02-FL19>
2022-04-13 15:12:31 -0500

    Added Test Data/ script

--- fifo_threaded/test_script.sh
+++ fifo_threaded/test_script.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-./client_fifo 1000 10 &
-./client_fifo 1010 10 &
-./client_fifo 1020 10 &
-./client_fifo 1030 10 &
-./client_fifo 1040 10 &
-./client_fifo 1150 10 &
+./client_fifo 300 10 &
+./client_fifo 200 10 &
+./client_fifo 500 50 &
+./client_fifo 500 98 &
+./client_fifo 3 99 p &
 exec bash
