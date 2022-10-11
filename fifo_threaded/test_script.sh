aef0e1ae864d05d3a147347c2d122aee1f8dc115
William Gozlan <william.gozlan@wustl.edu>
2022-10-11 00:27:09 -0400

    Added LITMUS traces and adjusted test script cases for it to avoid crashing

--- fifo_threaded/test_script.sh
+++ fifo_threaded/test_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-./client_fifo 300 10 &
+./client_fifo 100 10 & #300 10 &
 ./client_fifo 200 10 &
-./client_fifo 500 50 &
-./client_fifo 500 98 &
+./client_fifo 300 50 & #500 50 &
+./client_fifo 300 98 & #500 98 &
 ./client_fifo 3 99 p &
 exec bash
